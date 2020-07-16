d16f91a9718e64f63410e650316e2ab78070653d
saiteja623 <saiteja.balla13@gmail.com>
2020-07-16 11:03:45 +0530

    Updates

---
+++ precache-manifest.2e187120c88e8fdbcfd90c70671c13c1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a2527b8649aeb429b112fc21aaf4de",
+    "url": "/momentum/index.html"
+  },
+  {
+    "revision": "13f6de0890d7c0d47cd7",
+    "url": "/momentum/static/css/main.8c25f601.chunk.css"
+  },
+  {
+    "revision": "f1e0c0f9611a7966aa98",
+    "url": "/momentum/static/js/2.64a2b9fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/momentum/static/js/2.64a2b9fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f6de0890d7c0d47cd7",
+    "url": "/momentum/static/js/main.4a1fa37c.chunk.js"
+  },
+  {
+    "revision": "dd5edded77f39b117d48",
+    "url": "/momentum/static/js/runtime-main.de89ae0f.js"
+  },
+  {
+    "revision": "938e334e398ce45a293bb064c3e35b04",
+    "url": "/momentum/static/media/aft1.938e334e.jpg"
+  },
+  {
+    "revision": "c1672133f22c4ee5096076882b9f353e",
+    "url": "/momentum/static/media/aft2.c1672133.jpg"
+  },
+  {
+    "revision": "77544be1b392fe2073f7d6d3615acbec",
+    "url": "/momentum/static/media/aft3.77544be1.jpg"
+  },
+  {
+    "revision": "c91881efaec8674511252aac34f1043a",
+    "url": "/momentum/static/media/aft4.c91881ef.jpg"
+  },
+  {
+    "revision": "c8d50e6e14376fd136696e5c5a7875ab",
+    "url": "/momentum/static/media/evng1.c8d50e6e.jpg"
+  },
+  {
+    "revision": "6e660a2904d2f78f025f42f1392d0f7e",
+    "url": "/momentum/static/media/evng2.6e660a29.jpg"
+  },
+  {
+    "revision": "acf901568208f8ae0aa9329415534723",
+    "url": "/momentum/static/media/evng3.acf90156.jpg"
+  },
+  {
+    "revision": "799f7b9e89fa647b7e98c3c5c2167922",
+    "url": "/momentum/static/media/evng4.799f7b9e.jpg"
+  },
+  {
+    "revision": "776b02c6442a789314bcf2295e2ea357",
+    "url": "/momentum/static/media/mrng1.776b02c6.jpg"
+  },
+  {
+    "revision": "b30227c46d19e7e44502e9924ff40deb",
+    "url": "/momentum/static/media/mrng2.b30227c4.jpg"
+  },
+  {
+    "revision": "8d4bcf576881795706035b81f8729b1c",
+    "url": "/momentum/static/media/mrng3.8d4bcf57.jpg"
+  }
+]);
